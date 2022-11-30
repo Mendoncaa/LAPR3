cbe0a326713684984ddb5ccadc8bbc87a828adbf
Afonso Machado <1190326@isep.ipp.pt>
2022-11-30 19:22:36 +0000

    Test Source Root defined

---
+++ ESINF/src/test/java/test/ClientsProducersTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientsProducersTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void getLocationID() {
+    }
+
+    @Test
+    void getLatitude() {
+    }
+
+    @Test
+    void getLongitude() {
+    }
+
+    @Test
+    void getCode() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
