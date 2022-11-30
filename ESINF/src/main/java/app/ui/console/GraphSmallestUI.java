122dc8062c736d53451d5a3d8363a3dafe1e6bcc
Afonso Machado <1190326@isep.ipp.pt>
2022-11-30 01:03:22 +0000

    [US305] Done, Finally

--- ESINF/src/main/java/app/ui/console/GraphSmallestUI.java
+++ ESINF/src/main/java/app/ui/console/GraphSmallestUI.java
@@ -2,68 +2,111 @@ package app.ui.console;
 
 import app.controller.App;
 import app.domain.model.ClientsProducers;
+import app.domain.shared.DistanceComparator;
 import app.graph.Algorithms;
+import app.graph.Edge;
 import app.graph.map.MapGraph;
-import app.ui.console.utils.Utils;
+import app.graph.matrix.MatrixGraph;
 
 import java.util.*;
 
 public class GraphSmallestUI implements Runnable{
 
     MapGraph<ClientsProducers, Integer> clpGraph = App.getInstance().getCompany().getClientsProducersGraph();
-    ClientsProducers clp1 = null;
-    ArrayList<ClientsProducers> cp = clpGraph.vertices();
-    LinkedList<String> toBePrinted = new LinkedList<>();
-   ArrayList< LinkedList<ClientsProducers>> path = new ArrayList<>();
+    MapGraph<ClientsProducers, Integer> clpGraphClone = clpGraph.clone();
 
-   ArrayList<Integer> dists = new ArrayList<>();
-    ArrayList<String> codePath = new ArrayList<>();
+    MatrixGraph<ClientsProducers, Integer> clpMatrix;
+    MapGraph<ClientsProducers, Integer> mst = new MapGraph<>(false);
 
-    Comparator<Double> cpE;
+    ArrayList<Edge<ClientsProducers, Integer>> edgeArrayList = new ArrayList<>();
+
+    Iterator<ClientsProducers> cpIt = App.getInstance().getCompany().getClientsProducersGraph().vertices().iterator();
     @Override
     public void run() {
 
-        generateToBePrinted(cp, toBePrinted);
+        //Kruskal algorithm
+        parseGraph(clpGraphClone);
 
-        System.out.printf(toBePrinted + "\n");
+        ArrayList<ClientsProducers> clpcV = clpGraphClone.vertices();
 
-        String og = Utils.readLineFromConsole("Please Select Origin Vertex from list!\n");
+        for (ClientsProducers c :clpcV) {
+            mst.addVertex(c);
+        }
 
-        for(int i = 0; i < cp.size(); i++) {
+        Collection<Edge<ClientsProducers, Integer>> clpcE = clpGraphClone.edges();
 
-            if (og.equalsIgnoreCase(cp.get(i).getCode())) {
-                clp1 = cp.get(i);
+        edgeArrayList.addAll(clpcE);
+        Edge<ClientsProducers, Integer> edgebuffer;
+
+       // System.out.println(edgeArrayList);
+
+        //sorting array
+        for(int i = 0; i < edgeArrayList.size(); i++) {
+            for(int j = 1; j < edgeArrayList.size()-1; j++) {
+                if(compareEdges(edgeArrayList.get(i), edgeArrayList.get(j)) < 0){
+                    edgebuffer = edgeArrayList.get(i);
+                    edgeArrayList.set(i,edgeArrayList.get(j));
+                    edgeArrayList.set(j,edgebuffer);
+                }
             }
         }
 
-        System.out.println(clp1);
+        //System.out.println(edgeArrayList);
 
-        boolean sPathsResults = Algorithms.shortestPaths(clpGraph, clp1, Integer::compare, Integer::sum, 0, path, dists);
+        //removing duplicates
+        edgeArrayList.remove(0);
 
-        for(int i = 0; i < path.size(); i++) {
+        Iterator<Edge<ClientsProducers, Integer>> edgeIt = edgeArrayList.iterator();
+        Edge<ClientsProducers, Integer> previous = null;
+        Edge<ClientsProducers, Integer> current = null;
 
+        //System.out.println(edgeArrayList.size());
+
+        if (edgeIt.hasNext())
+        {
+            previous = edgeIt.next();
         }
 
-        System.out.printf("Smallest path from Origin to Destination is:" + codePath + " with size %d and distance in meters %d.\n", codePath.size(), sPathsResults);
+        while (edgeIt.hasNext())
+        {
+            current = edgeIt.next();
+            // Process previous and current here.
+            if(current.getVOrig().equals(previous.getVDest()) && current.getVDest().equals(previous.getVOrig())) {
+                edgeIt.remove();
+            }
+            // End of loop, after processing.  Maintain previous reference.
+            previous = current;
+        }
+        //System.out.println(edgeArrayList.size());
 
-        toBePrinted.clear();
-        path.clear();
-        codePath.clear();
+        //System.out.println(edgeArrayList);
+        LinkedList<ClientsProducers> connectedVerts = new LinkedList<>();
+
+        for (Edge<ClientsProducers, Integer> e : edgeArrayList) {
+            connectedVerts = Algorithms.DepthFirstSearch(mst, e.getVOrig());
+            if(!connectedVerts.contains(e.getVDest())){
+                mst.addEdge(e.getVOrig(), e.getVDest(), e.getWeight());
+            }
+        }
+
+        System.out.println(mst);
 
     }
+    private void parseGraph (MapGraph<ClientsProducers, Integer> clpGraphClone) {
 
-    private static void generateToBePrinted (ArrayList<ClientsProducers> cp, LinkedList<String> toBePrinted) {
+        while(cpIt.hasNext()) {
+            ClientsProducers c = cpIt.next();
 
-        for(int i = 0; i < cp.size(); i++) {
+            if (c.getType().equalsIgnoreCase("Empresa")) {
 
-            if(cp.get(i).getType().equalsIgnoreCase("Empresa")) {
-                continue;
+                clpGraphClone.removeVertex(c);
+
             }
-            String cpCodeBuffer = cp.get(i).getCode();
-
-            toBePrinted.add(cpCodeBuffer);
         }
+    }
+    public int compareEdges(Edge<ClientsProducers, Integer> e1, Edge<ClientsProducers, Integer> e2) {
 
+        return e1.getWeight().compareTo(e2.getWeight());
     }
 
 }
