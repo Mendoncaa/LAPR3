264700c50a983d355eeb43c8b32efff5c4b8e4da
David <1211572@isep.ipp.pt>
2022-12-02 14:23:07 +0000

    Main call Assembly functions

--- ARQCP/main.c
+++ ARQCP/main.c
@@ -30,6 +30,143 @@ int main() {
 
 
 }
+void SensTemp(){
+
+		char ult_temp=15;
+
+		for (int i = 0; i < 24; i++){
+		
+			char comp_rand = pcg32_random_r() % 3;
+		
+		
+		if(i!=0){
+			ult_temp=  *(ptrtemp-1)
+		}
+		
+		sens_temp(ult_temp, comp_rand);
+
+		*ptrtemp++;
+		
+	}
+
+	
+	
+	void SensVelcVento(){
+
+		char ult_velc_vento=10;
+
+		for (int i = 0; i < 24; i++){
+		
+			char comp_rand = pcg32_random_r() % 15;
+		
+		if(i!=0){
+			ult_velc_vento=  *(ptrvento-1);
+		}
+		
+		sens_velc_ventp(ult_velc_vento, comp_rand);
+
+		*ptrvento++;
+		
+	}
+	
+	void SensDirVento(){
+		//so pode variar entre 0 e 359
+
+		char ult_dir_vento=250;
+
+		for (int i = 0; i < 24; i++){
+		
+			char comp_rand = pcg32_random_r() % 15;
+
+		
+
+		if(i!=0){
+			ult_dir_vento=  *(ptrdir-1);
+		}
+		
+		sens_velc_vento(ult_dir_vento, comp_rand);
+
+		*ptrdir++;
+		
+	}
+	
+	void SensHumdAtm(){
+
+		char ult_dir_vento=250;
+
+		for (int i = 0; i < 24; i++){
+		
+			char comp_rand = pcg32_random_r() % 15;
+			
+		
+
+		if(i!=0){
+			ult_dir_vento=  *(ptrdir-1);
+		}
+		
+		sens_velc_vento(ult_dir_vento, comp_rand);
+
+		*ptrvento++;
+		
+	}
+
+	
+
+
+	//sensor pluvio
+
+
+	void SensPluvio(){
+
+		char ult_temp;
+
+    	*ptrtemp = *(ptrtemp-24);
+
+    	unsigned char ult_pluvio;
+		char pluvio[24];
+
+		char *ptrpluvio= pluvio
+
+    	
+
+
+    	for (int i = 0; i < 24; i++){
+
+        	char ult_temp = arraytemp[i];
+
+        	//sleep(k); //criar função para dar output de x em x segundos
+
+        	char comp_rand = pcg32_random_r() % 3; //mudar a alteração de temp
+			//componente aleatoria que gera um numero random, alteração
+
+        	int random_number = pcg32_random_r() % ult_temp ;
+			//
+
+
+        if (random_number !=0){ 	
+
+            comp_rand = 0;
+
+        }
+		// O valor só altera se a componente gerar o valor 0
+
+
+        if (i !=0){		
+
+            ult_pluvio = *(ptrpluvio-1);
+
+        }
+		// o ultimo valor da pluviosidade é a posição anterior para onde o apontador está a apontar
+
+
+
+        sens_pluvio(ult_pluvio,ult_temp, comp_rand);
+
+        *ptrpluvio++;
+
+    }
+	
+}
 
 for(int = 0; i<(sizeof(array__)/sizeof(array__[0]));i++){   //pluviosidade
 	sum += array__[i];
@@ -50,3 +187,4 @@ for(int = 0; i<(sizeof(array__)/sizeof(array__[0]));i++){   //pluviosidade
 	sum=0; 
 	count=0; 
 	avg=0;
+
