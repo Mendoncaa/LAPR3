284e2cce940f802d0420af14a6f88a97f76ee95c
DESKTOP-203BOC2\jpsaf <1211681@isep.ipp.pt>
2023-01-05 17:27:06 +0000

    [US213]- criação de Triggers e adição de tabela ao modelo físico

---
+++ BDDAD/US213.sql
@@ -0,0 +1,27 @@
+--Trigger que permite a utilização de inserts--
+CREATE OR REPLACE TRIGGER inPistaAuditoria
+    AFTER INSERT ON campo
+    FOR EACH ROW
+    BEGIN
+    INSERT INTO PistaAuditoria(campo_id,horaRealizada,tabela,tipo_alteracao) VALUES (utilizador, horaRealizada('now'),SYSTIMESTAMP,'INSERT')
+END;
+
+--Trigger que permite a utilização de update--
+CREATE OR REPLACE TRIGGER upPistaAuditoria
+    AFTER UPDATE ON campo
+    FOR EACH ROW
+    BEGIN
+    INSERT INTO PistaAuditoria(campo_id,horaRealizada,tabela,tipo_alteracao) VALUES (utilizador, horaRealizada('now'),SYSTIMESTAMP,'UPDATE')
+END;
+
+--Trigger que permite a utilização de delete--
+CREATE OR REPLACE TRIGGER inPistaAuditoria
+    AFTER DELETE ON campo
+    FOR EACH ROW
+    BEGIN
+    INSERT INTO PistaAuditoria(campo_id,horaRealizada,tabela,tipo_alteracao) VALUES (utilizador, horaRealizada('now'),SYSTIMESTAMP,'DELETE')
+END;
+
+
+select * from PistaAuditoria order by horaRealizada
+
