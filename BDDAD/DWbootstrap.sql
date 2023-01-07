8faa4ca27773ff554faa06bbf69e7709930edbde
windows <1211184@isep.ipp.pt>
2023-01-07 21:06:08 +0000

    US[214] final commit

--- BDDAD/DWbootstrap.sql
+++ BDDAD/DWbootstrap.sql
@@ -31,19 +31,19 @@ DBMS_OUTPUT.PUT_LINE('Setores');
 DBMS_OUTPUT.PUT_LINE('Expected: 5   Actual: '||nr_Setor_DIM);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Cultura(ID, Nome) VALUES (1,'Milho');
-insert into Cultura(ID, Nome) VALUES (2,'Tomate');
-insert into Cultura(ID, Nome) VALUES (3,'Batata');
-insert into Cultura(ID, Nome) VALUES (4,'Cebola');
+insert into Cultura_H(ID, Nome) VALUES (1,'Milho');
+insert into Cultura_H(ID, Nome) VALUES (2,'Tomate');
+insert into Cultura_H(ID, Nome) VALUES (3,'Batata');
+insert into Cultura_H(ID, Nome) VALUES (4,'Cebola');
 DBMS_OUTPUT.PUT_LINE('Culturas');
-SELECT Count(*) INTO nr_cultura from Cultura;
+SELECT Count(*) INTO nr_cultura from Cultura_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 4   Actual: '||nr_cultura);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Tipo_Cultura(ID, Designacao) VALUES (1,'Permanente');
-insert into Tipo_Cultura(ID, Designacao) VALUES (2,'Temporaria');
+insert into Tipo_Cultura_H(ID, Designacao) VALUES (1,'Permanente');
+insert into Tipo_Cultura_H(ID, Designacao) VALUES (2,'Temporaria');
 DBMS_OUTPUT.PUT_LINE('Tipo Culturas');
-SELECT Count(*) INTO nr_tipo_cultura from Tipo_Cultura;
+SELECT Count(*) INTO nr_tipo_cultura from Tipo_Cultura_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 2   Actual: '||nr_tipo_cultura);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
@@ -73,26 +73,26 @@ SELECT Count(*) INTO nr_tempo_dim from Tempo_DIM;
 DBMS_OUTPUT.PUT_LINE('Expected: 12   Actual: '||nr_tempo_dim);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Morada(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (1,'Rua 1', 1234-1234, 'Localidade 1', 'Portugal', 1);
-insert into Morada(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (2,'Rua 2', 1234-1234, 'Localidade 2', 'Portugal', 2);
-insert into Morada(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (3,'Rua 3', 1234-1234, 'Localidade 3', 'Portugal', 3);
-insert into Morada(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (4,'Rua 4', 1234-1234, 'Localidade 4', 'Portugal', 4);
+insert into Morada_H(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (1,'Rua 1', 1234-1234, 'Localidade 1', 'Portugal', 1);
+insert into Morada_H(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (2,'Rua 2', 1234-1234, 'Localidade 2', 'Portugal', 2);
+insert into Morada_H(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (3,'Rua 3', 1234-1234, 'Localidade 3', 'Portugal', 3);
+insert into Morada_H(ID, Nome_Rua, Codigo_Postal, Localidade, Pais, numero_porta) VALUES (4,'Rua 4', 1234-1234, 'Localidade 4', 'Portugal', 4);
 DBMS_OUTPUT.PUT_LINE('Moradas');
-SELECT Count(*) INTO nr_morada from Morada;
+SELECT Count(*) INTO nr_morada from Morada_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 4   Actual: '||nr_morada);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Tipo_Cliente(ID, Designacao) VALUES (1,'Tipo 1');
-insert into Tipo_Cliente(ID, Designacao) VALUES (2,'Tipo 2');
+insert into Tipo_Cliente_H(ID, Designacao) VALUES (1,'Tipo 1');
+insert into Tipo_Cliente_H(ID, Designacao) VALUES (2,'Tipo 2');
 DBMS_OUTPUT.PUT_LINE('Tipo Clientes');
-SELECT Count(*) INTO nr_tipo_cliente from Tipo_Cliente;
+SELECT Count(*) INTO nr_tipo_cliente from Tipo_Cliente_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 2   Actual: '||nr_tipo_cliente);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Morada_Entrega_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (1,'Cliente 1', 1, 1, 1, 123456789, 'c1@gmail.com', 1000, 1, 100);
-insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Morada_Entrega_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (2,'Cliente 2', 2, 2, 2, 123456711, 'c2@gmail.com', 2000, 2, 200);
-insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Morada_Entrega_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (3,'Cliente 3', 3, 1, 3, 123456333, 'c3@gmail.com', 3000, 3, 300);
-insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Morada_Entrega_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (4,'Cliente 4', 4, 2, 4, 123454444, 'c4@gmail.com', 4000, 4, 400);
+insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (1,'Cliente 1', 1, 1, 123456789, 'c1@gmail.com', 1000, 1, 100);
+insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (2,'Cliente 2', 2, 2, 123456711, 'c2@gmail.com', 2000, 2, 200);
+insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (3,'Cliente 3', 3, 1, 123456333, 'c3@gmail.com', 3000, 3, 300);
+insert into Cliente_DIM(Codigo_Unico, Nome, Morada_Correspondencia_ID, Tipo_Cliente_ID, Numero_Fiscal, Email, Plafond, Numero_Total_Encomendas, Valor_Total_Encomendas) VALUES (4,'Cliente 4', 4, 2, 123454444, 'c4@gmail.com', 4000, 4, 400);
 DBMS_OUTPUT.PUT_LINE('Clientes');
 SELECT Count(*) INTO nr_cliente_dim from Cliente_DIM;
 DBMS_OUTPUT.PUT_LINE('Expected: 4   Actual: '||nr_cliente_dim);
@@ -102,18 +102,18 @@ DBMS_OUTPUT.PUT_LINE(chr(0));
 --US216
 --acceptance criteria 3
 
-insert into Tipo_Hub(ID, Designacao) VALUES (1,'E');
-insert into Tipo_Hub(ID, Designacao) VALUES (2,'P');
+insert into Tipo_Hub_H(ID, Designacao) VALUES (1,'E');
+insert into Tipo_Hub_H(ID, Designacao) VALUES (2,'P');
 DBMS_OUTPUT.PUT_LINE('Tipo Hubs');
-SELECT Count(*) INTO nr_tipo_hub from Tipo_Hub;
+SELECT Count(*) INTO nr_tipo_hub from Tipo_Hub_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 2   Actual: '||nr_tipo_hub);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
-insert into Hub (Location_ID, Latitude, Longitude) VALUES ('CT14',38.5243,-8.8926);
-insert into Hub (Location_ID, Latitude, Longitude) VALUES ('CT11',39.3167,-7.4167);
-insert into Hub (Location_ID, Latitude, Longitude) VALUES ('CT10',39.7444,-8.8072);
+insert into Hub_H (Location_ID, Latitude, Longitude) VALUES ('CT14',38.5243,-8.8926);
+insert into Hub_H (Location_ID, Latitude, Longitude) VALUES ('CT11',39.3167,-7.4167);
+insert into Hub_H (Location_ID, Latitude, Longitude) VALUES ('CT10',39.7444,-8.8072);
 DBMS_OUTPUT.PUT_LINE('Hubs');
-SELECT Count(*) INTO nr_hub from Hub;
+SELECT Count(*) INTO nr_hub from Hub_H;
 DBMS_OUTPUT.PUT_LINE('Expected: 3   Actual: '||nr_hub);
 DBMS_OUTPUT.PUT_LINE(chr(0));
 
